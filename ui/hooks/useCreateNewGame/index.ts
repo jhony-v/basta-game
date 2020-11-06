91fb79ce0b6a1c36ebbe9ab7373c76165f5a8279
Jhony Vega <jhonyvegacuya24@gmail.com>
2020-11-06 09:10:14 -0500

    feat: Create method to getRandomLetter

--- ui/hooks/useCreateNewGame/index.ts
+++ ui/hooks/useCreateNewGame/index.ts
@@ -3,22 +3,26 @@ import { gamingActions } from "../../../features/gaming";
 import { firebaseServiceCreateNewRoom } from "../../../services/FirebaseService";
 
 
-const getRandomLetterFromAlphabet = () => {
+const randomLetterFromAlphabet = () => {
     const codeA = "a".charCodeAt(0);
     const codeZ = "z".charCodeAt(0);
     const len = (codeZ+1) - codeA;
     const createLetters = Array(len).fill("").map((_,index) => {
         return String.fromCharCode(codeA+index);
     })
-    const randomIndexBetweenAZ = Math.floor(Math.random() * len);
-    return createLetters[randomIndexBetweenAZ];
+    return () => {
+        const randomIndexBetweenAZ = Math.floor(Math.random() * len);
+        return createLetters[randomIndexBetweenAZ];
+    }
 }
+
+const getRandomLetter = randomLetterFromAlphabet();
 
 const useCreateNewGame = () => {
     const dispatch = useDispatch();
     const createNewGame = () => {
         const id = (Math.random().toString(8)+Date.now()).replace("0.","").substring(0,10);
-        const randomLetter = getRandomLetterFromAlphabet();
+        const randomLetter = getRandomLetter();
         const data = {
             id,
             gaming:false,
